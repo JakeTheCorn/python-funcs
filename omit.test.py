d41b9c8a14724a66c1eb3fce04f1b9e316b20f75
Jake Corn <jakethecorn@gmail.com>
2020-04-12 20:04:53 -0400

    modularize omit

--- omit.test.py
+++ omit.test.py
@@ -1,29 +1,5 @@
 import unittest
-import copy
-
-FALLBACK_FLAG = '____FALLBACK_FLAG'
-
-def omit(obj, paths):
-  if not isinstance(obj, dict):
-    return obj
-  collector = copy.deepcopy(obj)
-  for path in paths:
-    if '.' not in path:
-      if collector.get(path):
-        del collector[path]
-      continue
-    sub_paths = path.split('.')
-
-    if len(sub_paths) < 2:
-      continue
-
-    first, rest = sub_paths[0], sub_paths[1:]
-    val = collector.get(first, FALLBACK_FLAG)
-    if val == FALLBACK_FLAG:
-      continue
-    collector[first] = omit(val, rest)
-
-  return collector
+from omit import omit
 
 class TestOmit(unittest.TestCase):
   def test_simple(self):
